f31d56ad051da993a2a6feca386e7b2306e3f0ba
ArthurNsp <37721@raphaeldisanto.com.br>
2025-08-26 10:56:28 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Xuxa1 from './assets/notebook-xuxa.jpg'
+import hellsing from 'hellsing.jpg'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,16 +13,19 @@ function App() {
         <section className="produtos">
 
           {/* Produto 1 */}
-          <div className="card">
-            <img src={Xuxa1} alt="" className="img-card" />
-            <h2 className="titulo-card">Notebook Xuxa</h2>
-            <p className="desc">Notebook Gamer com RGB 1 nucleo e 512Mb de Ram</p>
-            <p className="preco">R$ 5.999,99</p>
-            <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
-            <div className="off">- 10%</div>
-          </div>
+          <a href="#" className="link-card">
+            <div className="card">
+              <img src={Xuxa1} alt="" className="img-card" />
+              <h2 className="titulo-card">Notebook Xuxa</h2>
+              <p className="desc">Notebook Gamer com RGB 1 nucleo e 512Mb de Ram</p>
+              <p className="preco">R$ 5.999,99</p>
+              <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
+              <div className="off">- 10%</div>
+            </div>
+          </a>
 
           {/* Produto 2 */}
+          <a header="#" className="img-card">
           <div className="card">
             <img src={Xuxa1} alt="" className="img-card" />
             <h2 className="titulo-card">Notebook Xuxa</h2>
@@ -30,17 +34,18 @@ function App() {
             <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
             <div className="off">- 10%</div>
           </div>
-
+          </a>
           {/* Produto 3 */}
-          <div className="card">
-            <img src={Xuxa1} alt="" className="img-card" />
-            <h2 className="titulo-card">Notebook Xuxa</h2>
-            <p className="desc">Notebook Gamer com RGB 1 nucleo e 512Mb de Ram</p>
-            <p className="preco">R$ 5.999,99</p>
-            <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
-            <div className="off">- 10%</div>
-          </div>
-
+          <a header="#" className="link-card">
+            <div className="card">
+              <img src={Xuxa1} alt="" className="img-card" />
+              <h2 className="titulo-card">Notebook Xuxa</h2>
+              <p className="desc">Notebook Gamer com RGB 1 nucleo e 512Mb de Ram</p>
+              <p className="preco">R$ 5.999,99</p>
+              <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
+              <div className="off">- 10%</div>
+            </div>
+          </a>
         </section>
         <section className="destaque"></section>
         <footer></footer>
