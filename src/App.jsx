dea560692fccaacba1f615b217b9b898d5920a9e
ArthurNsp <37721@raphaeldisanto.com.br>
2025-09-01 09:42:54 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import hellsing from 'hellsing.jpg'
-
+import hellsing from './assets/hellsing.jpg'
+import HP from './assets/hp.jpg'
+import HxH from './assets/HxH.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,39 +16,42 @@ function App() {
           {/* Produto 1 */}
           <a href="#" className="link-card">
             <div className="card">
-              <img src={Xuxa1} alt="" className="img-card" />
-              <h2 className="titulo-card">Notebook Xuxa</h2>
-              <p className="desc">Notebook Gamer com RGB 1 nucleo e 512Mb de Ram</p>
-              <p className="preco">R$ 5.999,99</p>
+              <img src={hellsing} alt="" className="img-card" />
+              <h2 className="titulo-card">Coleção completa Hellsing</h2>
+              <p className="desc">Coleção de 1 á 10 (todos os volumes) </p>
+              <p className="preco">R$ 499,99</p>
               <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
-              <div className="off">- 10%</div>
+             
             </div>
           </a>
 
           {/* Produto 2 */}
           <a header="#" className="img-card">
           <div className="card">
-            <img src={Xuxa1} alt="" className="img-card" />
-            <h2 className="titulo-card">Notebook Xuxa</h2>
-            <p className="desc">Notebook Gamer com RGB 1 nucleo e 512Mb de Ram</p>
-            <p className="preco">R$ 5.999,99</p>
-            <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
-            <div className="off">- 10%</div>
+            <img src={HP} alt="" className="img-card" />
+            <h2 className="titulo-card">Coleção completa Hell's paradise</h2>
+            <p className="desc">todos os volume 1 a 13</p>
+            <p className="preco">R$ 250,00</p>
+            <div className="avaliacao"> ★ ★ ★ ★ ★</div>
+            
           </div>
           </a>
           {/* Produto 3 */}
           <a header="#" className="link-card">
             <div className="card">
-              <img src={Xuxa1} alt="" className="img-card" />
-              <h2 className="titulo-card">Notebook Xuxa</h2>
-              <p className="desc">Notebook Gamer com RGB 1 nucleo e 512Mb de Ram</p>
-              <p className="preco">R$ 5.999,99</p>
-              <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
+              <img src={HxH} alt="" className="img-card" />
+              <h2 className="titulo-card">coleção semi-nova Hunter x Hunter</h2>
+              <p className="desc">Coleçao com 1 ano de vida (estão como novos)</p>
+              <p className="preco">R$  ̶(2̶.̶2̶5̶0̶,̶0̶0̶) 2.025,00</p>
+              <div className="avaliacao"> ★ ★ ★ ☆ ☆</div>
               <div className="off">- 10%</div>
             </div>
           </a>
         </section>
         <section className="destaque"></section>
+
+        <iframe src="https://www.youtube.com/embed/p7cDN1ofIw0?autoplay=1&mute=1&controls=0&loop=1&playlist=p7cDN1ofIw0&modestbranding=1&showinfo=0” frameborder="0" allow="autoplay; fullscreen"></iframe>
+
         <footer></footer>
       </main>
     </>
