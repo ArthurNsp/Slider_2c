5614ae7ee55910bed7a323ffe08d0688c7380aa7
ArthurNsp <37721@raphaeldisanto.com.br>
2025-09-01 09:44:47 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
         </section>
         <section className="destaque"></section>
 
-        <iframe src="https://www.youtube.com/embed/p7cDN1ofIw0?autoplay=1&mute=1&controls=0&loop=1&playlist=p7cDN1ofIw0&modestbranding=1&showinfo=0” frameborder="0" allow="autoplay; fullscreen"></iframe>
+        <iframe src="https://www.youtube.com/embed/p7cDN1ofIw0?autoplay=1&mute=1&controls=0&loop=1&playlist=p7cDN1ofIw0&modestbranding=1&showinfo=0" frameborder="0" allow="autoplay; fullscreen"></iframe>
 
         <footer></footer>
       </main>
